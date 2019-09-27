8b87aa62fd37368c732df7bbba10a310d64d5a4f
Randy W. Sims <simsrw73@gmail.com>
2019-09-27 10:28:08 -0400

    fix: set transform origin on element, not in js

--- src/app/app.js
+++ src/app/app.js
@@ -9,13 +9,13 @@ let ticking = false;
 window.addEventListener('scroll', function scrollEvt() {
     const logo = document.querySelector('#logo-full');
     const pos = window.scrollY;
-    logo.style.transformOrigin = '0 0';
+    // logo.style.transformOrigin = '0 0';
     if (logo && !ticking) {
         window.requestAnimationFrame(function animateLogo() {
             const opacity = 1 - pos / 400;
             logo.style.opacity = opacity.toString();
             const angle = pos / 30;
-            if (angle < 5) {
+            if (angle < 4) {
                 logo.style.transform = `skewY(${-angle}deg)`;
             }
             ticking = false;
