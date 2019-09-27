ea683be50dc4fa6a04afc86d45647241e028a34f
Randy W. Sims <simsrw73@gmail.com>
2019-09-27 19:08:45 -0400

    refactor: generate service cards from json file

--- pug.config.js
+++ pug.config.js
@@ -1,9 +1,13 @@
 const packageJSON = require('./package.json');
 
+const data = {};
+data.services = require('./src/data/services.json');
+
 module.exports = {
     locals: {
         name: packageJSON.name,
         desc: packageJSON.description,
         author: packageJSON.author,
+        data,
     },
 };
