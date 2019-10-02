135f4f2eac9c3bb9ba67f7b1e4f230615ef44a1d
Randy W. Sims <simsrw73@gmail.com>
2019-10-02 15:46:16 -0400

    fix: improve metadata passed to pug

--- pug.config.js
+++ pug.config.js
@@ -5,9 +5,11 @@ data.services = require('./src/data/services.json');
 
 module.exports = {
     locals: {
-        name: packageJSON.name,
-        desc: packageJSON.description,
-        author: packageJSON.author,
+        name: 'A Anytime Anywhere',
+        description: '24 Hour Mobile Truck & Trailer Repair',
+        author: 'A Anytime Anywhere of Georgia, LLC.',
+        website: 'https://a24.newagain.site',
+        packageJSON,
         data,
     },
 };
