c5392303adbe1800d4d8ae183df0458275a1dd08
Randy W. Sims <simsrw73@gmail.com>
2020-06-10 14:37:07 -0400

    add google tag id

--- pug.config.js
+++ pug.config.js
@@ -9,6 +9,7 @@ module.exports = {
         description: '24 Hour Mobile Truck & Trailer Repair',
         author: 'A Anytime Anywhere of Georgia, LLC.',
         website: 'https://a24.newagain.site',
+        gtag: 'UA-18436925-2',
         packageJSON,
         data,
     },
