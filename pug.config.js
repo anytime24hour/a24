07e3effe67e2145c577ea2275ddc8d9f8a6f5bdc
Randy W. Sims <simsrw73@gmail.com>
2020-06-29 13:11:40 -0400

    Configure for production site

--- pug.config.js
+++ pug.config.js
@@ -3,13 +3,16 @@ const packageJSON = require('./package.json');
 const data = {};
 data.services = require('./src/data/services.json');
 
+const GTAG_anytime24hour = 'UA-18436925-3';
+const GTAG_a24 = 'UA-18436925-2';
+
 module.exports = {
     locals: {
         name: 'A Anytime Anywhere',
-        description: '24 Hour Mobile Truck & Trailer Repair',
-        author: 'A Anytime Anywhere of Georgia, LLC.',
-        website: 'https://a24.newagain.site',
-        gtag: 'UA-18436925-2',
+        description: '24 Hour Mobile Truck & Trailer Repair in Georgia',
+        author: 'A Anytime Anywhere 24 Hour Mobile Truck Repair of Georgia, LLC',
+        website: 'https://anytime24hour.com',
+        gtag: GTAG_anytime24hour,
         packageJSON,
         data,
     },
